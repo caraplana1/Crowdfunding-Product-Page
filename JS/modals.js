84b92787f825cef8187c2fa4eae2d9991f971b7b
caraplana1 <cristianlandaez@gmail.com>
2022-02-09 12:00:31 -0400

    modal display completed

---
+++ JS/modals.js
@@ -0,0 +1,9 @@
+function DisplayModal(){
+	let modal = document.getElementById("backModal")
+	modal.style.display = "block"
+}
+
+function UndisplayModal(){
+	let modal = document.getElementById("backModal")
+	modal.style.display = "none"
+}
