e0efa3fd1d6c8ac66e54d1753c1fee097ec3a86d
caraplana1 <cristianlandaez@gmail.com>
2022-02-23 17:39:07 -0400

    radio checking completed

--- JS/modals.js
+++ JS/modals.js
@@ -7,3 +7,18 @@ function UndisplayModal(){
 	let modal = document.getElementById("modal")
 	modal.style.display = "none"
 }
+
+function ActivateRadioCheckers(e){
+	let radios = document.getElementsByClassName("radio")
+	
+	for (let i = 0; i < radios.length; i++) {
+		const radio = radios[i];
+		if (radio.id == e.target.id || radio.children[0] == e.target)
+		{
+			radio.children[0].style.display = "block";
+		}
+		else{
+			radio.children[0].style.display = "none";
+		}
+	}
+}
